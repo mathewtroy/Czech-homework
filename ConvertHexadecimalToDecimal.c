d5d376ddebee5e0a530fa1996541a4ba9c2d81f0
mathewtroy <75392252+mathewtroy@users.noreply.github.com>
2021-03-01 18:46:27 +0300

    Update ConvertHexadecimalToDecimal.c

--- ConvertHexadecimalToDecimal.c
+++ ConvertHexadecimalToDecimal.c
@@ -2,7 +2,7 @@
 #include <math.h>
 #include <string.h>
 
-int main() {
+int main(){
     long long decimalCislo=0;
     char hexCislo[16] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
     char hexadecimal[30];
@@ -10,7 +10,7 @@ int main() {
     printf("Zadejte sestnactkove cislo:\n");
     scanf("%s", hexadecimal);
 
-    for(i=strlen(hexadecimal)-1; i >= 0; i--) {
+    for(i=strlen(hexadecimal)-1; i >= 0; i--){
         for(j=0; j<16; j++){
             if(hexadecimal[i] == hexCislo[j]){
                 decimalCislo += j*pow(16, power);
